0d6d3cf845be279c9915b75ee3aaa5f57cbec597
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-04-04 14:12:50 +0530

    Navbar component added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,20 @@ export default function Navbar() {
 
 
   return (
-    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-sm'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
-            <div className='flex items-center'>
+            <div className='flex items-center pl-2'>
                 {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Dribbble</h1> */}
-                <img className='w-[50px] cursor-pointer' src={logo} alt="" /> humankind
+                <img className='w-[50px] cursor-pointer font-semibold' src={logo} alt="" /> <span className='font-bold cursor-pointer'>humankind</span>
             </div>
-            <div className='hidden md:flex pr-6'>
-                <ul className='hidden md:flex pl-10 space-x-4 '>
-                    <li className='cursor-pointer'>Art</li>
-                    <li className='cursor-pointer'>Collectibles</li>
-                    <li className='cursor-pointer'>Stdios</li>
-                    <li className='cursor-pointer'>Community</li>
-                    <li className='cursor-pointer'>Subscribe</li>
-                    <button className='border-none bg-black text-white rounded-lg p-1 border-r-8  '>Burn a lunchbox</button>
+            <div className='hidden md:flex pr-2 mt-2'>
+                <ul className='hidden md:flex pl-8 space-x-8 '>
+                    <li className='cursor-pointer font-semibold'>Art</li>
+                    <li className='cursor-pointer font-semibold'>Collectibles</li>
+                    <li className='cursor-pointer font-semibold'>Stdios</li>
+                    <li className='cursor-pointer font-semibold'>Community</li>
+                    <li className='cursor-pointer font-semibold'>Subscribe</li>
+                    <button className='border-none font-semibold bg-black rounded-full mb-2 text-white p-2'>Burn a Lunchbox</button>
                 </ul>
             </div>
             <div className='md:hidden' onClick={handleClick}>
@@ -30,15 +30,15 @@ export default function Navbar() {
         </div>
 
 
-        <ul className={!nav?'hidden':'absolute bg-zinc-200 w-full px-8'}>
-            <li className='border-b-2 border-zinc-300 w-full'>Art</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Collectibles</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Stdios</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Community</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Subcribe</li>
+        <ul className={!nav?'hidden':'absolute bg-white w-full px-8 space-y-4'}>
+            <li className='border-b-2 border-zinc-500 w-full text-center'>Art</li>
+            <li className='border-b-2 border-zinc-500 w-full text-center'>Collectibles</li>
+            <li className='border-b-2 border-zinc-500 w-full text-center'>Stdios</li>
+            <li className='border-b-2 border-zinc-500 w-full text-center'>Community</li>
+            <li className='border-b-2 border-zinc-500 w-full text-center'>Subcribe</li>
 
             <div className='flex flex-col my-4'>
-                <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Burn a Lunchbox</button>
+                <button className='bg-black text-white  px-8 py-3 mb-4'>Burn a Lunchbox</button>
             </div>
         </ul>
     </div>
