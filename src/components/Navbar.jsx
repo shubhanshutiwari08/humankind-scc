4b5a431a043cb36c3a0fecc94a66cd373afd61a4
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-04-04 13:53:56 +0530

    updated nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,22 +8,21 @@ export default function Navbar() {
 
 
   return (
-    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
+    <div className='w-screen h-[80px] z-10 bg-white fixed drop-shadow-lg'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center'>
                 {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Dribbble</h1> */}
-                <img className='w-[50px] cursor-pointer' src={logo} alt="" /> Humankind
-                <ul className='hidden md:flex '>
-                    <li className='cursor-pointer'>Inspiration</li>
-                    <li className='cursor-pointer'>Find work</li>
-                    <li className='cursor-pointer'>learn Design</li>
-                    <li className='cursor-pointer'>Go pro</li>
-                    <li className='cursor-pointer'>Hire Designers</li>
-                </ul>
+                <img className='w-[50px] cursor-pointer' src={logo} alt="" /> humankind
             </div>
-            <div className='hidden md:flex pr-4'>
-                <button className='border-none bg-transparent text-black mr-4'>Sign in</button>
-                <button className='px-8 py-3'>Sign up</button>
+            <div className='hidden md:flex pr-6'>
+                <ul className='hidden md:flex pl-10 space-x-4 '>
+                    <li className='cursor-pointer'>Art</li>
+                    <li className='cursor-pointer'>Collectibles</li>
+                    <li className='cursor-pointer'>Stdios</li>
+                    <li className='cursor-pointer'>Community</li>
+                    <li className='cursor-pointer'>Subscribe</li>
+                    <button className='border-none bg-black text-white rounded-lg p-1 border-r-8  '>Burn a lunchbox</button>
+                </ul>
             </div>
             <div className='md:hidden' onClick={handleClick}>
                 {!nav ? <MenuIcon className='w-5' />:<XIcon className='w-5'/>}
@@ -32,15 +31,14 @@ export default function Navbar() {
 
 
         <ul className={!nav?'hidden':'absolute bg-zinc-200 w-full px-8'}>
-            <li className='border-b-2 border-zinc-300 w-full'>Inspiration</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Find work</li>
-            <li className='border-b-2 border-zinc-300 w-full'>learn Design</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Go pro</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Hire Designers</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Art</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Collectibles</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Stdios</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Community</li>
+            <li className='border-b-2 border-zinc-300 w-full'>Subcribe</li>
 
             <div className='flex flex-col my-4'>
-                <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Sign in</button>
-                <button className='px-8 py-3'>Sign up</button>
+                <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4'>Burn a Lunchbox</button>
             </div>
         </ul>
     </div>
