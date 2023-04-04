4de52328da2bfaedad752612cd9250b087b263bc
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-04-04 11:43:09 +0530

    initiated the tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
-    <h1>This is gonna be the clone for humankind website</h1>
+    <h1 className="text-3xl font-bold underline">This is gonna be the clone for humankind website</h1>
     </>
   );
 }
