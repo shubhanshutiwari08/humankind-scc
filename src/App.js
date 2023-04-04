02be36bf3203c9d8926535a006f059aa0cb09698
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-04-04 11:58:05 +0530

    nav added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
     <h1 className="text-3xl font-bold underline">This is gonna be the clone for humankind website</h1>
     </>
   );
