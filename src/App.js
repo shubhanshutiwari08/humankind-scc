249c7d8f42f08583c2204485c2692274a1230466
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-04-05 09:25:00 +0530

    home created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
     <Navbar/>
-    <h1 className="text-3xl font-bold underline">This is gonna be the clone for humankind website</h1>
+    <Home/>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import human from "../assets/human.png"
+import kind from "../assets/kind.png"
+
+
+export default function Home() {
+  return (
+    <>
+    <img className='item-center' src={kind} alt=""  />
+    </>
+  )
+}
